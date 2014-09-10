11640e0ab9ba1596790a9a3a6abf1663b5b32e2b
Poulo Ghost <864984007@qq.com>
2014-09-10 23:04:07 +0800

    add all record utils
    change record invoke and queryforrecord in tables

--- src/gt/high5/database/table/RawRecord.java
+++ src/gt/high5/database/table/RawRecord.java
@@ -1,12 +1,27 @@
 package gt.high5.database.table;
 
+import gt.high5.core.provider.PackageProvider;
 import gt.high5.core.service.RecordContext;
 import gt.high5.database.model.Table;
 
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
+/**
+ * @author GT
+ * 
+ *         raw record table
+ */
 public class RawRecord {
 	public static String TYPE_DAY_OF_MONTH = "DayOfMonth";
 	public static String TYPE_LAST_PACKAGE = "LastPackage";
@@ -18,18 +33,216 @@ public class RawRecord {
 	public static String TYPE_WEEK_DAY = "WeekDay";
 	public static String TYPE_WIFI_NAME = "WifiName";
 
+	// time region length in minutes
+	private static int regionLength = 15;
+
+	public static void setRegionLength(int length) {
+		regionLength = length;
+	}
+
+	/**
+	 * @author GT
+	 * 
+	 *         operation for recording a certain type of record
+	 */
 	interface RecordOperation {
 		public Object queryForRecord(RecordContext context);
 
-		public Object queryForRead(RecordContext context);
+		// public Object queryForRead(RecordContext context);
 
 		public Class<?> getType();
 	}
 
+	/**
+	 * mapping type name to operation
+	 */
 	private static HashMap<String, RecordOperation> recordOperations = new HashMap<String, RawRecord.RecordOperation>();
 	static {
+		recordOperations.put(TYPE_DAY_OF_MONTH, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				return Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+			}
+
+			// @Override
+			// public Object queryForRead(RecordContext context) {
+			// return queryForRecord(context);
+			// }
+
+			@Override
+			public Class<?> getType() {
+				return Integer.class;
+			}
+		});
+
+		recordOperations.put(TYPE_LAST_PACKAGE, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				PackageProvider provider = null;
+				List<String> order = null;
+				if (null != (provider = PackageProvider
+						.getPackageProvider(context.getContext()))
+						&& null != (order = provider
+								.getLastPackageOrder(context.getContext()))) {
+					Total total = context.getTotal();
+					int index = order.indexOf(total.getName());
+					// exists and not last one
+					if (-1 != index && order.size() - 1 != index) {
+						return order.get(index + 1);
+					}
+				}
+				return null;
+			}
+
+			// @Override
+			// public Object queryForRead(RecordContext context) {
+			// // TODO Auto-generated method stub
+			// return null;
+			// }
+
+			@Override
+			public Class<?> getType() {
+				return String.class;
+			}
+		});
+
+		recordOperations.put(TYPE_NETWORK, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				ConnectivityManager manager = (ConnectivityManager) context
+						.getContext().getSystemService(
+								Context.CONNECTIVITY_SERVICE);
+				if (null != manager) {
+					NetworkInfo info = manager.getActiveNetworkInfo();
+					if (null == info || (!info.isConnectedOrConnecting())) {
+						return "NONE";
+					} else {
+						return info.getTypeName() + "_" + info.getSubtypeName();
+					}
+				} else {
+					return null;
+				}
+			}
+
+			@Override
+			public Class<?> getType() {
+				return String.class;
+			}
+		});
+
+		recordOperations.put(TYPE_RING_MODE, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				return ((AudioManager) context.getContext().getSystemService(
+						Context.AUDIO_SERVICE)).getMode();
+			}
+
+			@Override
+			public Class<?> getType() {
+				return Integer.class;
+			}
+		});
+
+		recordOperations.put(TYPE_RING_VOLUMN, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				AudioManager manager = (AudioManager) context.getContext()
+						.getSystemService(Context.AUDIO_SERVICE);
+				int type = AudioManager.STREAM_RING;
+				double max = manager.getStreamMaxVolume(type);
+				double current = manager.getStreamVolume(type);
+				return (int) (current / max * 10);
+			}
+
+			@Override
+			public Class<?> getType() {
+				return Integer.class;
+			}
+		});
+
+		recordOperations.put(TYPE_TIME, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				Calendar calendar = Calendar.getInstance();
+				int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60
+						+ calendar.get(Calendar.MINUTE);
+				return (int) (minutes / regionLength);
+			}
+
+			@Override
+			public Class<?> getType() {
+				return Integer.class;
+			}
+		});
+
+		recordOperations.put(TYPE_TOTAL, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				return context.getTotal().getName();
+			}
+
+			@Override
+			public Class<?> getType() {
+				return String.class;
+			}
+		});
+
+		recordOperations.put(TYPE_WEEK_DAY, new RecordOperation() {
+			// week day
+			private final int[] DAYS_INDEX = { Calendar.SUNDAY,
+					Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY,
+					Calendar.THURSDAY, Calendar.FRIDAY, Calendar.SATURDAY, };
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				Calendar calendar = Calendar.getInstance();
+				int dayValue = calendar.get(Calendar.DAY_OF_WEEK);
+				return Arrays.binarySearch(DAYS_INDEX, dayValue);
+			}
+
+			@Override
+			public Class<?> getType() {
+				return Integer.class;
+			}
+		});
+
+		recordOperations.put(TYPE_WIFI_NAME, new RecordOperation() {
+
+			@Override
+			public Object queryForRecord(RecordContext context) {
+				WifiManager manager = (WifiManager) context.getContext()
+						.getSystemService(Context.WIFI_SERVICE);
+				if (null != manager) {
+					WifiInfo info = manager.getConnectionInfo();
+					String bssid = info.getBSSID();
+					if (null == bssid) {
+						if (manager.isWifiEnabled()) {
+							bssid = "ON";
+						} else {
+							bssid = "OFF";
+						}
+					}
+					return bssid;
+				} else {
+					return null;
+				}
+			}
+
+			@Override
+			public Class<?> getType() {
+				return String.class;
+			}
+		});
 	}
 
+	// store values in hashmap
 	private HashMap<String, Object> mValues = new HashMap<String, Object>();
 
 	public RawRecord() {
